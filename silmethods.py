6000c1c4bbecd4932ee004a1dcd70a05c2fa174d
Your Name <you@example.com>
2023-11-28 15:17:07 +0100

    :test

--- silmethods.py
+++ silmethods.py
@@ -2,12 +2,12 @@ from random import randint
 
 
 def silgame(getal):
-    gamech= randint(0,100)
+    gamech= randint(0,3)
     #mynum= input("Enter your number between 0 and 100 here: ")
     mynum= int(getal)
     int(mynum)
 
     if gamech==mynum:
-        print("Yay, you guessed right")
+        return "Yay, you guessed right"
     else:
-        print("That's a shame, you lost")
+        return "That's a shame, you lost"
